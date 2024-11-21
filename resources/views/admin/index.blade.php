9818a86855fe68456499ed5126d883048ad4693f
Vodila <vodila@servitor.solutions>
2024-11-21 18:05:11 +0200

    ep.5.addAdminTemplate

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,20 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+    @include('admin.css')
 </head>
 <body>
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
-
-
-</x-app-layout>
-
+@include('admin.header')
+@include('admin.sidebar')
+@include('admin.body')
+@include('admin.footer')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/ttt',[AdminController::class,'ttt']);
+
 Route::get('/',[AdminController::class,'home']);
 
 Route::get('/home',[AdminController::class,'index'])->name('home');
