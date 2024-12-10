9f6035dea7347a5dfc53a008feeaaa539029bd46
Vodila <vodila@servitor.solutions>
2024-12-10 17:21:22 +0200

    ep.26.addMiddlewareAdmin

--- routes/web.php
+++ routes/web.php
@@ -16,26 +16,36 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [AdminController::class, 'home'])->name('home');
 
-Route::get('/rooms/create', [AdminController::class, 'create'])->name('rooms.create');
-Route::get('/rooms/show', [AdminController::class, 'show'])->name('rooms.show');
-Route::post('/rooms', [AdminController::class, 'store'])->name('rooms.store');
-Route::get('/rooms_delete/{id}', [AdminController::class, 'delete'])->name('rooms.delete');
-Route::get('/rooms_edit/{id}', [AdminController::class, 'edit'])->name('rooms.edit');
-Route::patch('/rooms_update/{id}', [AdminController::class, 'update'])->name('rooms.update');
-Route::get('/home', [AdminController::class, 'index'])->name('home');
+
+
+
+
+
+
+
 
 Route::get('/room_details/{id}', [HomeController::class, 'details'])->name('room.details');
 Route::post('/add_booking/{id}', [HomeController::class, 'add_booking']);
-Route::get('/bookings', [AdminController::class, 'bookings']);
-Route::get('/delete_booking/{booking}', [AdminController::class, 'delete_booking']);
 
-Route::get('/approve_booking/{booking}', [AdminController::class, 'approve_booking']);
-Route::get('/reject_booking/{booking}', [AdminController::class, 'reject_booking']);
-Route::get('/view_gallary', [AdminController::class, 'view_gallary']);
-Route::post('/upload_gallary', [AdminController::class, 'upload_gallary']);
-Route::get('/delete_gallary/{id}', [AdminController::class, 'delete_gallary']);
 Route::post('/contact', [HomeController::class, 'contact']);
-Route::get('/all_messages', [AdminController::class, 'all_messages']);
-Route::get('/send_mail/{contact}', [AdminController::class, 'send_mail']);
-Route::post('/mail/{contact}', [AdminController::class, 'mail']);
+Route::middleware(['auth','admin'])->group(function () {
+    Route::get('/rooms/create', [AdminController::class, 'create'])->name('rooms.create');
+    Route::get('/rooms/show', [AdminController::class, 'show'])->name('rooms.show');
+    Route::post('/rooms', [AdminController::class, 'store'])->name('rooms.store');
+    Route::get('/rooms_delete/{id}', [AdminController::class, 'delete'])->name('rooms.delete');
+    Route::get('/rooms_edit/{id}', [AdminController::class, 'edit'])->name('rooms.edit');
+    Route::patch('/rooms_update/{id}', [AdminController::class, 'update'])->name('rooms.update');
+    Route::get('/home', [AdminController::class, 'index'])->name('home');
+    Route::get('/bookings', [AdminController::class, 'bookings']);
+    Route::get('/delete_booking/{booking}', [AdminController::class, 'delete_booking']);
+    Route::get('/approve_booking/{booking}', [AdminController::class, 'approve_booking']);
+    Route::get('/reject_booking/{booking}', [AdminController::class, 'reject_booking']);
+    Route::get('/view_gallary', [AdminController::class, 'view_gallary']);
+    Route::post('/upload_gallary', [AdminController::class, 'upload_gallary']);
+    Route::get('/delete_gallary/{id}', [AdminController::class, 'delete_gallary']);
+    Route::get('/all_messages', [AdminController::class, 'all_messages']);
+    Route::get('/send_mail/{contact}', [AdminController::class, 'send_mail']);
+    Route::post('/mail/{contact}', [AdminController::class, 'mail']);
+
+});
 
